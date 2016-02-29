4279cb4fdf4d52816aca69d24aad74af12ce1243
Matthew Ames <git@matty.digital>
2016-02-29 19:58:25 +0000

    no data needed in the page

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php
+    wp_head();
+    get_header();
+?>
+
+<div class="container contentblock">
+<div class="row spacer">
+<?php
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+    ?>
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+            <h2><?php the_title(); ?></h2>
+            <hr>
+            <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+<?php
+    endwhile;
+    else:
+        _e('Sorry, no posts matched your criteria.');
+    endif;
+?>
+</div>
+</div>
+
+<?php
+    get_footer();
+    wp_footer();
+?>
