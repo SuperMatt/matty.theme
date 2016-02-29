4b48114f2f494c1fba6aebe61720884fc71a5b5a
Matthew Ames <git@matty.digital>
2016-02-29 19:44:52 +0000

    shrunk width to 100%

--- page-templates/home.php
+++ page-templates/home.php
@@ -21,7 +21,7 @@
     <div class="col-md-4">
         <div class="panel panel-default">
             <div class="panel-body">
-                <?php the_post_thumbnail('4col', array('class' => 'img-rounded', 'width' => '100%')); ?>
+                <?php the_post_thumbnail('4col', array('class' => 'img-rounded', 'style' => 'width: 100%')); ?>
             </div>
         </div>
     </div>
