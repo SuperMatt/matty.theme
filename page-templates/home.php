52611a31a6c2a5020f5c8da97b87f55ff33627d4
Matthew Ames <git@matty.digital>
2016-02-29 19:49:31 +0000

    resized columns

--- page-templates/home.php
+++ page-templates/home.php
@@ -11,14 +11,14 @@
 <?php
     if ( have_posts() ) : while ( have_posts() ) : the_post();
     ?>
-    <div class="col-md-8">
+    <div class="col-md-9">
         <div class="panel panel-default">
             <div class="panel-body">
                 <?php the_content(); ?>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-3">
         <div class="panel panel-default">
             <div class="panel-body">
                 <?php the_post_thumbnail('4col', array('class' => 'img-rounded img-responsive')); ?>
