9871ed78fd41f01d26c22e654a8b23be41a1452c
Matthew Ames <git@matty.digital>
2016-02-29 19:16:17 +0000

    home page

---
+++ page-Home.php
@@ -0,0 +1,20 @@
+<div class="container contentblock">
+<div class="row spacer">
+<?php
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+    ?>
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+            <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+<?php
+    endwhile;
+    else:
+        _e('Sorry, no posts matched your criteria.');
+    endif;
+?>
+</div>
+</div>
