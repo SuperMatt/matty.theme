05f055a4060f1656825329d508787576b677d255
Matthew Ames <git@matty.digital>
2016-03-02 22:17:30 +0000

    added 100% back to menu

--- footer.php
+++ footer.php
@@ -5,11 +5,11 @@
 <script>
     $( document ).ready(function() {
         $('#dropdown-parent').on('show.bs.dropdown', function() {
-            $("html, body").css("overflow", "hidden");
+            $("html, body").css({"overflow": "hidden", "height": "100%");
         });
 
         $('#dropdown-parent').on('hide.bs.dropdown', function() {
-            $("html, body").css("overflow", "auto");
+            $("html, body").css({"overflow": "auto", "height": "auto"});
         });
     });
 </script>
