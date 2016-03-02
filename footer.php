e99be3a1216ef45ddb661981fa0bcfc0ae431bfe
Matthew Ames <git@matty.digital>
2016-03-02 22:26:34 +0000

    disable touch move

--- footer.php
+++ footer.php
@@ -6,6 +6,9 @@
     $( document ).ready(function() {
         $('#dropdown-parent').on('show.bs.dropdown', function() {
             $("body").css("overflow", "hidden");
+            $("body").on("touchstart touchmove", function(e) {
+                e.preventDefault();
+            });
         });
 
         $('#dropdown-parent').on('hide.bs.dropdown', function() {
