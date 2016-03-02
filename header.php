ff5c1e350254086b150612d07e486e11c9c31080
Matthew Ames <git@matty.digital>
2016-03-02 21:53:29 +0000

    dropdown menu

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <link rel='stylesheet' href="<?php echo get_template_directory_uri(); ?>/wordpress.css">
     <link rel='stylesheet' href="<?php echo get_template_directory_uri(); ?>/style.css">
     <script>
-        $(document).ready(function() {
+        $( document ).ready(function() {
             $('#dropdownmenu').on('shown.bs.dropdown', function() {
                 alert('open');
                 $("html, body").css("overflow", "hidden");
