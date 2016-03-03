8bd9db5f803b52dc1f381ac4586d6ceb2da0c36a
Matthew Ames <git@matty.digital>
2016-03-03 12:42:40 +0000

    shrunk down to one page type

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <link href='https://fonts.googleapis.com/css?family=Raleway:400,500|Open+Sans|Comfortaa' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <title><?php the_title() ?></title>
+    <title><?php bloginfo('name'); ?></title>
     <link rel='stylesheet' href="<?php echo get_template_directory_uri(); ?>/wordpress.css">
     <link rel='stylesheet' href="<?php echo get_template_directory_uri(); ?>/style.css">
   </head>
--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 ?>
 
 <div class="container contentblock">
-<div class="row spacer">
 <?php
     if ( have_posts() ) : while ( have_posts() ) : the_post();
     ?>
+    <div class="row spacer">
     <div class="col-md-9">
         <div class="panel panel-default">
             <div class="panel-body">
-            <h2><?php the_title(); ?></h2>
-            <p class="text-muted"><?php the_date(); ?> - <?php the_time(); ?></p>
+            <h1><a href="<?php get_permalink(); ?>"><?php the_title(); ?></h1>
+            <p class="text-muted">Last updated: <?php the_date(); ?> - <?php the_time(); ?></p>
             <hr>
             <?php the_content(); ?>
             </div>
@@ -21,6 +21,7 @@
     <div class="col-md-3">
         <div class="panel panel-default">
             <div class="panel-body">
+                <center><?php the_post_thumbnail('4col', array('class' => 'img-responsive')); ?></center>
                 <?php
                 if ( function_exists( 'sharing_display' ) ) {
                     sharing_display( '', true );
@@ -34,13 +35,13 @@
             </div>
         </div>
     </div>
+    </div>
 <?php
     endwhile;
     else:
         _e('Sorry, no posts matched your criteria.');
     endif;
 ?>
-</div>
 </div>
 
 <?php
