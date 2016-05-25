0ad3f35593ab3c9d38d274a88c91c106e8a9fe5d
Matthew Ames <git@matty.digital>
2016-05-25 14:36:06 +0100

    opengraph info

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
             query_posts('posts_per_page=1');
         }
         if ( have_posts() ) : while ( have_posts() ) : the_post();
-                ?><meta property="og:description" content="<?php echo htmlspecialchars(get_the_content()); ?>"><?php
+                ?><meta property="og:description" content="<?php echo substr(htmlspecialchars(get_the_content()), 300); ?>"><?php
             endwhile;
         endif;
         wp_reset_query();
