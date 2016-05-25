1774d9a231a69fc44d621be30660a47e9a05e76b
Matthew Ames <git@matty.digital>
2016-05-25 10:56:27 +0100

    added roboto mono for code

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@ if (is_single()): ?>
         <script id="dsq-count-scr" src="https://mattydigital.disqus.com/count.js" async></script>
     </body>
 <!--  -->
-<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400|Roboto+Mono:100,300,400' rel='stylesheet' type='text/css'>
 <link rel='stylesheet' href="<?php echo get_template_directory_uri(); ?>/wordpress.css">
 <?php wp_head(); ?>
 <?php wp_footer(); ?>
