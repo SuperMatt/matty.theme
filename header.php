4fb66726737d95f0c929bcb179c4d56e89c96ec7
Matthew Ames <git@matty.digital>
2016-05-25 13:22:52 +0100

    adding structured data

--- header.php
+++ header.php
@@ -3,6 +3,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php bloginfo('name'); ?></title>
         <link rel='stylesheet' href="<?php echo get_template_directory_uri(); ?>/style.css">
+        <script type="application/ld+json">
+        {
+          "@context": "http://schema.org/",
+          "@type": "Person",
+          "name": "Matthew Ames",
+          "jobTitle": "Systems Administrator",
+          "url": "https://matty.digital"
+        }
+        </script>
     </head>
     <body>
         <div class="main shadow-light">
