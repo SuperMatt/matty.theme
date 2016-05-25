a4f4f6ac16cfc52c1c58c62368f336a4c800b6cb
Matthew Ames <git@matty.digital>
2016-05-25 11:21:04 +0100

    moved style sheet to footer

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php bloginfo('name'); ?></title>
-        <link rel='stylesheet' href="<?php echo get_template_directory_uri(); ?>/style.css" media="bogus">
     </head>
     <body>
         <div class="main shadow-light">
