f05a36fb47942b565e00bb99c7e506498600540a
Matthew Ames <git@matty.digital>
2016-05-25 11:23:58 +0100

    moved style sheet back to header

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php bloginfo('name'); ?></title>
+        <link rel='stylesheet' href="<?php echo get_template_directory_uri(); ?>/style.css">
     </head>
     <body>
         <div class="main shadow-light">
